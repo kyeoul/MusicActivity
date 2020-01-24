ac85b03d340883269a7105050c352a54536a9b85
kyeoul <insertwinterhere@gmail.com>
2020-01-24 12:01:37 -0600

    oaweufhwoefuh

---
+++ app/src/main/java/com/example/musicactivity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.musicactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+    }
+
+    public void onClick(View v) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.nocturn_play);
+        mediaPlayer.start();
+    }
+
+}
